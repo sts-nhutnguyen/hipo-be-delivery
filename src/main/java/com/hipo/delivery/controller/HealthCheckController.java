71bc9b9ffbaefd008907b27907722da28d46b2fa
STS\quyen.nguyen <quyen.nguyen@saigontechnology.com>
2022-12-27 15:35:33 +0700

    Update fixes by the followed instructions on PR

--- src/main/java/com/hipo/delivery/controller/HealthCheckController.java
+++ src/main/java/com/hipo/delivery/controller/HealthCheckController.java
@@ -1,12 +1,13 @@
-package com.hipo_delivery_be.controller;
+package com.hipo.delivery.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @RequestMapping(value = "/api/helloworld")
+    @GetMapping(value = "/api/helloworld")
     public String hello() {
         return "Hello world";
     }
